ebd70e95313da1e1ab9968a5439db963d8bc1b80
Nox9 <yhandesir01@gmail.com>
2025-12-20 00:49:34 +0800

    API init

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,11 +10,11 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 import adminRoutes from "./routes/admin.route.js";
-// import userRoutes from "./routes/user.route.js";
-// import orderRoutes from "./routes/order.route.js";
-// import reviewRoutes from "./routes/review.route.js";
-// import productRoutes from "./routes/product.route.js";
-// import cartRoutes from "./routes/cart.route.js";
+import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 // import paymentRoutes from "./routes/payment.route.js";  
 
 const app = express();
@@ -41,11 +41,11 @@ app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/orders", orderRoutes);
-// app.use("/api/reviews", reviewRoutes);
-// app.use("/api/products", productRoutes);
-// app.use("/api/cart", cartRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success inside the server nnow!!!! wooohhhhh" });
