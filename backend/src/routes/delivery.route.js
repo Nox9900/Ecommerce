543b7c05d32236bad52722e50b118dd3ca8364d1
yhan <yhandesir01@gmail.com>
2026-02-12 00:38:55 +0800

    delivery route error fixed

--- backend/src/routes/delivery.route.js
+++ backend/src/routes/delivery.route.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { updateTrackingInfo, getDeliveryInfo } from "../controllers/delivery.controller.js";
-import { protectRoute } from "../middleware/protectRoute.js";
-import { requireAdmin } from "../middleware/requireAdmin.js";
+import { protectRoute, adminOnly } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // Update tracking info (admin only)
-router.put("/:orderId/tracking", protectRoute, requireAdmin, updateTrackingInfo);
+router.put("/:orderId/tracking", protectRoute, adminOnly, updateTrackingInfo);
 
 // Get delivery info (authenticated users - will check ownership in controller)
 router.get("/:orderId", protectRoute, getDeliveryInfo);
